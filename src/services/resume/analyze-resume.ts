dcd5a3d1560459b281453fbf8057fe3527ca7e51
1montandon <emontandon11@gmail.com>
2025-05-25 22:40:11 -0300

    feat: update Analyses model and migration to remove extractedText and resume_id columns

--- src/services/resume/analyze-resume.ts
+++ src/services/resume/analyze-resume.ts
@@ -1,27 +1,24 @@
 import { generateText } from "ai";
 import { google } from "../../config/geminiai";
-import fs from 'fs'
-
-
+import fs from "fs";
 
 export async function analyzeResume(resumePath: string) {
-
-const result = await generateText({
-  model: google,
-  messages: [
-    {
-      role: 'user',
-      content: [
-        { type: 'text', text: 'What is the file about?' },
-        {
-          type: 'file',
-          mimeType: 'application/pdf',
-          data: fs.readFileSync(resumePath),
-          filename: 'example.pdf', // optional, not used by all providers
-        },
-      ],
-    },
-  ],
-});
-    return result
+  const result = await generateText({
+    model: google,
+    messages: [
+      {
+        role: "user",
+        content: [
+          { type: "text", text: "What is the file about?" },
+          {
+            type: "file",
+            mimeType: "application/pdf",
+            data: fs.readFileSync(resumePath),
+            filename: "example.pdf", // optional, not used by all providers
+          },
+        ],
+      },
+    ],
+  });
+  return result;
 }
