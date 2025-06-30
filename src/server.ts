e13965178a528a2921ecaaf3cbf0a3e17c8a60f3
emontandon11@gmail.com <emontandon11@gmail.com>
2025-06-30 13:13:29 -0300

    feat: update dependencies and switch database provider to PostgreSQL

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
 import { errorHandler } from './middlewares/error-handler';
+import cors from 'cors';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json()) ;
+app.use(cors())
 app.use('/api', routes)
 app.use(errorHandler)
 app.listen(PORT, '0.0.0.0', () => {
